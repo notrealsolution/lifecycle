7b8cb86e0099f03b04e52bb87e69dc16d17287f9
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-06-12 07:57:13 -0600

    Implementando unsubscribe

--- src/app/products/pages/price/price.component.ts
+++ src/app/products/pages/price/price.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price-page',
@@ -10,19 +11,24 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   @Input()
   public price: number = 0;
 
+  public interval$?: Subscription;
 
   constructor() {
     console.log('**COMPONENTE HIJO**')
   }
 
   ngOnInit():void {
+
     console.log('PriceComponent(HIJO): ngOnInit');
+
+    this.interval$ = interval(1000).subscribe( value => console.log(`Tick: ${ value }`) );
   }
   ngOnChanges(changes: SimpleChanges): void {
     console.log('PriceComponent(HIJO): ngOnChanges');
     console.log({ changes });
   }
   ngOnDestroy(): void {
+    this.interval$?.unsubscribe();
     console.log('PriceComponent(HIJO): ngOnDestroy');
   }
 
